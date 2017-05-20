74dcf9b41c9cd2b7aceebee2a492f7228e935e80
evanlinacre <evanlinacre@gmail.com>
2017-05-19 22:01:20 -0400

    Made it actually a game

--- ApEngProj/APEngProj/APEngProj/ScreenManager.cs
+++ ApEngProj/APEngProj/APEngProj/ScreenManager.cs
@@ -23,11 +23,15 @@ namespace APEngProj
         public static Texture2D boulder;
         public static Texture2D progressBar;
         public static Texture2D progressBarFull;
+        public static Texture2D gameOver;
         public static KeyboardState oldState, newState;
         public static float rotation;
         public static int hillOffset;
         public static float timeSinceSpace;
         public static float progress;
+        public static string mode;
+        public static int boulderOffset;
+        public static int gameOverCounter;
         public static Dictionary<string, Screen> Screens;
 
         public ScreenManager()
@@ -50,6 +54,9 @@ namespace APEngProj
             rotation = 0;
             hillOffset = 0;
             progress = 0;
+            boulderOffset = 0;
+            gameOverCounter = 0;
+            mode = "start";
         }
 
         /// <summary>
@@ -72,6 +79,7 @@ namespace APEngProj
             boulder = Content.Load<Texture2D>("boulder");
             progressBar = Content.Load<Texture2D>("progress");
             progressBarFull = Content.Load<Texture2D>("progressFull");
+            gameOver = Content.Load<Texture2D>("gameOver");
 
         }
 
@@ -92,31 +100,79 @@ namespace APEngProj
         /// <param name="gameTime">Provides a snapshot of timing values.</param>
         protected override void Update(GameTime gameTime)
         {
-            newState = Keyboard.GetState();
-
-            if (oldState.IsKeyUp(Keys.Space) && newState.IsKeyDown(Keys.Space))
+            if (mode == "start")
             {
-                timeSinceSpace = 0;
-            } else
-            {
-                timeSinceSpace += gameTime.ElapsedGameTime.Milliseconds;
-            }
+                newState = Keyboard.GetState();
 
-            if (timeSinceSpace < 200)
-            {
-                progress += gameTime.ElapsedGameTime.Milliseconds;
-                sisyphus.Update();
-
-                rotation += MathHelper.Pi / 150;
-
-                hillOffset += 1;
-                if (hillOffset >= 600 / .9f)
+                if (newState.IsKeyDown(Keys.Space))
                 {
-                    hillOffset = 0;
+                    mode = "game";
                 }
             }
+            if (mode == "game")
+            {
+                newState = Keyboard.GetState();
 
-            oldState = newState;
+                if (oldState.IsKeyUp(Keys.Space) && newState.IsKeyDown(Keys.Space))
+                {
+                    timeSinceSpace = 0;
+                }
+                else
+                {
+                    timeSinceSpace += gameTime.ElapsedGameTime.Milliseconds;
+                }
+
+                if (timeSinceSpace < 200)
+                {
+                    progress += gameTime.ElapsedGameTime.Milliseconds;
+                    sisyphus.Update();
+
+                    rotation += MathHelper.Pi / 150;
+
+                    hillOffset += 1;
+                    if (hillOffset >= 600 / .9f)
+                    {
+                        hillOffset = 0;
+                    }
+                }
+                else
+                {
+                    mode = "falling";
+                }
+
+                if (progress >= 59000)
+                {
+                    mode = "falling";
+                }
+
+                oldState = newState;
+            }
+            else if (mode == "falling")
+            {
+                rotation -= MathHelper.Pi / 40;
+                boulderOffset += 5;
+                if (boulderOffset > 528)
+                {
+                    mode = "gameOver";
+                }
+            }
+            else if (mode == "gameOver")
+            {
+                gameOverCounter++;
+                if (gameOverCounter >= 300)
+                {
+                    mode = "reset";
+                }
+            }
+            else if (mode == "reset")
+            {
+                rotation = 0;
+                hillOffset = 0;
+                progress = 0;
+                boulderOffset = 0;
+                gameOverCounter = 0;
+                mode = "start";
+            }
 
             base.Update(gameTime);
         }
@@ -130,12 +186,38 @@ namespace APEngProj
             GraphicsDevice.Clear(Color.CornflowerBlue);
 
             spriteBatch.Begin();
-
-            spriteBatch.Draw(hill, new Vector2(-hillOffset * 8f / 3f * .45f, -480 + hillOffset * .45f), Color.White);
-            spriteBatch.Draw(boulder, new Vector2(528, 220), new Rectangle(0, 0, 128, 128), Color.White, rotation, new Vector2(64, 64), 1.0f, SpriteEffects.None, 0f);
-            sisyphus.Draw(spriteBatch, new Vector2(400, 200));
-            spriteBatch.Draw(progressBar, new Vector2(200, 420), Color.White);
-            spriteBatch.Draw(progressBarFull, new Vector2(200, 420), new Rectangle(0, 0, (int)(progress / 60000f * 400f), 20), Color.White);
+            if (mode == "start")
+            {
+                spriteBatch.Draw(hill, new Vector2(-hillOffset * 8f / 3f * .45f, -480 + hillOffset * .45f), Color.White);
+                spriteBatch.Draw(boulder, new Vector2(528, 220), new Rectangle(0, 0, 128, 128), Color.White, rotation, new Vector2(64, 64), 1.0f, SpriteEffects.None, 0f);
+                sisyphus.Draw(spriteBatch, new Vector2(400, 200));
+                spriteBatch.Draw(progressBar, new Vector2(200, 420), Color.White);
+                spriteBatch.Draw(progressBarFull, new Vector2(200, 420), new Rectangle(0, 0, (int)(progress / 60000f * 400f), 20), Color.White);
+            }
+            if (mode == "game")
+            {
+                spriteBatch.Draw(hill, new Vector2(-hillOffset * 8f / 3f * .45f, -480 + hillOffset * .45f), Color.White);
+                spriteBatch.Draw(boulder, new Vector2(528, 220), new Rectangle(0, 0, 128, 128), Color.White, rotation, new Vector2(64, 64), 1.0f, SpriteEffects.None, 0f);
+                sisyphus.Draw(spriteBatch, new Vector2(400, 200));
+                spriteBatch.Draw(progressBar, new Vector2(200, 420), Color.White);
+                spriteBatch.Draw(progressBarFull, new Vector2(200, 420), new Rectangle(0, 0, (int)(progress / 60000f * 400f), 20), Color.White);
+            }
+            else if (mode == "falling")
+            {
+                spriteBatch.Draw(hill, new Vector2(-hillOffset * 8f / 3f * .45f, -480 + hillOffset * .45f), Color.White);
+                spriteBatch.Draw(boulder, new Vector2(528 - boulderOffset * 8f / 3f * .45f, 220 + boulderOffset * .45f), new Rectangle(0, 0, 128, 128), Color.White, rotation, new Vector2(64, 64), 1.0f, SpriteEffects.None, 0f);
+                sisyphus.Draw(spriteBatch, new Vector2(400, 200));
+                spriteBatch.Draw(progressBar, new Vector2(200, 420), Color.White);
+                spriteBatch.Draw(progressBarFull, new Vector2(200, 420), new Rectangle(0, 0, (int)(progress / 60000f * 400f), 20), Color.White);
+            }
+            else if (mode == "gameOver")
+            {
+                spriteBatch.Draw(hill, new Vector2(-hillOffset * 8f / 3f * .45f, -480 + hillOffset * .45f), Color.White);
+                sisyphus.Draw(spriteBatch, new Vector2(400, 200));
+                spriteBatch.Draw(progressBar, new Vector2(200, 420), Color.White);
+                spriteBatch.Draw(progressBarFull, new Vector2(200, 420), new Rectangle(0, 0, (int)(progress / 60000f * 400f), 20), Color.White);
+                spriteBatch.Draw(gameOver, new Vector2(0, 0), new Color(Color.White, gameOverCounter / 100f));
+            }
             spriteBatch.End();
 
             base.Draw(gameTime);
